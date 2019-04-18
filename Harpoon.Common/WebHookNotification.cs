1b740f20245071108a05ac306a7a812f4b742821
romain v <rvergnory@lucca.fr>
2019-04-18 15:05:00 +0200

    MOD : use netstandard for most packages

--- Harpoon.Common/WebHookNotification.cs
+++ Harpoon.Common/WebHookNotification.cs
@@ -5,7 +5,6 @@ namespace Harpoon
     public class WebHookNotification : IWebHookNotification
     {
         public string ActionId { get; set; }
-        public WebHookAction Action { get; set; }
 
         public Dictionary<string, object> Payload { get; set; }
         IReadOnlyDictionary<string, object> IWebHookNotification.Payload => Payload;
